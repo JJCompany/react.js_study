640595e2e008dbf0638633599dcd3506fef2414a
JJCompany <wc_s-p@daum.net>
2020-03-25 17:43:53 +0900

    study component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn test
-        </a>
-      </header>
-    </div>
-  );
+/* 
+    Component는 하나의 최상위 태그로 구성되어야 한다. 
+    jsx로 작성을 하면 create-react-app이 컨버팅을 해준다.
+    React는 코드를 정리해주는 역할도 같이 해준다. -> 복잡도를 획기적으로 낮추었다.
+*/
+class Subject extends Component{
+    render(){//render 함수
+        return(
+            <header>
+                <h1>{this.props.title}</h1>
+                {this.props.sub}
+            </header>
+        );
+    }
+}
+
+//table of contents
+class TOC extends Component{
+    render(){
+        return(
+            <nav>
+                <ul>
+                    <li><a href="">1</a></li>
+                    <li><a href="">2</a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+class Content extends Component{
+    render(){
+        return(
+            <article>
+                <h2>{this.props.title}</h2>
+                {this.props.desc}
+            </article>
+        );
+    }
+}
+
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <Subject title="Web" sub="world wid Web!!!!"></Subject>
+                <Subject title="React" sub="study react"></Subject>
+                <TOC></TOC>
+                <Content title="HTML" desc="HTML is hyperText Markup!!"></Content>
+            </div>
+        );
+    }
 }
 
 export default App;
