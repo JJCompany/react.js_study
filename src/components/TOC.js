49625d9cca2837781c1f578db6a45555921ef03b
JJCompany <wc_s-p@daum.net>
2020-03-25 18:41:13 +0900

    study component props and state

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 //table of contents
 class TOC extends Component{
     render(){
+        var lists = [];
+        var data = this.props.data
+        var i = 0;
+        while(i<data.length){
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            i++;
+        }
+
         return(
             <nav>
                 <ul>
-                    <li><a href="">1</a></li>
-                    <li><a href="">2</a></li>
+                    {lists}
                 </ul>
             </nav>
         );
